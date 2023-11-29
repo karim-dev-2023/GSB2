117cf1807614bffff874beedea53d4e3604d99c7
karim-dev-2023 <131853616+karim-dev-2023@users.noreply.github.com>
2023-11-29 22:17:30 +0100

    V 0.2

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,4 +1,10 @@
 
 public class Controleur {
 
+//	Les Attributs 
+	private V_principal v_principal;
+	
+	public Controleur() {
+		this.v_principal = new V_principal();
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		V_principale vP = new V_principale();
+		
+		Controleur c = new Controleur();
 		
 	}
 }
